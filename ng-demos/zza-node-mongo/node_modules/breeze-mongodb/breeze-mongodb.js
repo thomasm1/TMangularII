0b6f0703cc5d7eac889a91edec3f4f1940fe7910
Thomas Maestas <Thomas.Maestas@syncsort.com>
2018-09-25 23:47:42 -0600

    reinitialize fixed tmangularii

--- ng-demos/zza-node-mongo/node_modules/breeze-mongodb/breeze-mongodb.js
+++
@@ -1,6 +0,0 @@
-var queryModule = require("./mongoQuery");
-var saveModule = require("./mongoSaveHandler");
-
-exports.MongoQuery = queryModule.MongoQuery;
-exports.MongoSaveHandler = saveModule.MongoSaveHandler;
-
