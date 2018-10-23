7dbea63728488ca5d58aebe7740262d4a5aa2e0c
Thomas Maestas <Thomas.Maestas@syncsort.com>
2018-10-23 10:50:17 -0600

    finish ngtype2

--- jasmine-karma-templates/1-angularjs-ngmock-unit-testing-m1-exercise-files/after/movie-app/spec/calculator.spec.js
+++
@@ -1,5 +0,0 @@
-describe('calculator', function() {
-	it('should add two numbers', function() {
-		expect(add(1,2)).toBe(3);
-	});
-});
--- jasmine-karma-templates/5-angularjs-ngmock-unit-testing-m5-exercise-files/before/movie-app/src/movie-app/search.controller.js
+++
@@ -1,8 +0,0 @@
-angular.module('movieApp')
-	.controller('SearchController', function($scope, $location) {
-		$scope.search = function() {
-			if ($scope.query) {
-				$location.path('/results').search('q', $scope.query);
-			}
-		}
-	});
