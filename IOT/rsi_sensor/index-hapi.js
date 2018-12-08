cd761fd8cdbcab050c91f81633eddbf3bb2c9f86
Thomas Maestas <thomas76milton@gmail.com>
2018-12-08 08:37:34 -0700

    awsRouting, firebase-host

--- IOT/rsi_sensor/index-hapi.js
+++ IOT/rsi_sensor/index-hapi.js
@@ -1,31 +1,35 @@
 'use strict';
 
-const Hapi = require('hapi');
+const Hapi=require('hapi');
 
 // Create a server with a host and port
-const serverHapi = Hapi.server({
-    host: 'localhost',
-    port: 8000
+const server=Hapi.server({
+    host:'localhost',
+    port:8000
 });
 
-//Add the route.
+// Add the route
 server.route({
     method:'GET',
     path:'/hello',
-    handler: function(request, h) {
-	return 'hello world';
+    handler:function(request,h) {
+
+        return'hello world';
     }
 });
 
-//Start the server
+// Start the server
 async function start() {
-	
+
     try {
-	await serverHapi.start();
+        await server.start();
     }
     catch (err) {
-  	console.log(err);
-	process.exit(1);
-    };
+        console.log(err);
+        process.exit(1);
+    }
+
+    console.log('Server running at:', server.info.uri);
+};
 
 start();
