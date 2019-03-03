42ae5a59dce36e68cfd2e8a2d73b65f76f896e3f
Thomas M. Maestas <thomasm1@users.noreply.github.com>
2019-03-03 11:15:08 -0700

    Adding in partitions by scrape type + scrape title

--- _TMM/tmm-sensor/public/app/app-scraper.js
+++ _TMM/tmm-sensor/public/app/app-scraper.js
@@ -1,24 +1,57 @@
-var app = angular
-    .module("SensorScraper", [])
-    .controller("tierScrapeController", function ($scope, $rootScope) {
-        $rootScope.rootScopeScraper = "Root Scope Scraper";
-        $scope.tierScrape = "Tier Scrape";
-    })
-    .controller("nadirScrapeController", function ($scope) {
-        $scope.nadirScrape = "Nadir Scrape";
-    })a
-    .controller("midController", function ($http, $route) {
-        var vm = this;
+// $scope version
+
+var app = angular.module("SensorScraper", []);  
+    // app.config(function ($provide) {
+    //     $provide.factory("scrape", function () {  
+    // $provide.factory is shorthand for $provide.provider, 
+    // which sets up a more generic provider returning objects that have $get fxs
+
+     app.provider("scrape", function () {
+            var type;
+            return {
+                setType: function (value) {
+                   type = value;
+                },
+                $get: function () {
+                   return  { 
+                       title: type + "SensorScraper"
+                   };
+                }
+            };
+     }); 
+
+    app.controller("tierScrapeCtrl", function ($scope, $rootScope, scrape) {
+        $rootScope.rootScopeScraper = "Root Scrape";
         
-        vm.reloadData = function () {
+        $scope.tierScrapeTitle = "Tier Scrape";
+        $scope.title = scrape.title + $scope.tierScrapeTitle
+    });
+
+    app.controller("nadirScrapeCtrl", function ($scope, scrape) {
+        $scope.nadirScrapeTitle = "Nadir Scrape";
+        $scope.title = scrape.title + $scope.nadirScrape;
+    });
+
+    app.controller("midScrapeCtrl", function ($http, $route, $q, $log, $scope, scrape) { 
+        $scope.midScrapeTitle = "Mid Scrape";
+        $scope.title = scrape.title + $scope.midScrapeTitle;
+        
+        $scope.reloadData = function () {
             $route.reload();
         }
-        $http.get("dataScrape.asmx/GetAllScrapes")
+        $http.get("api/getAllScrapes")
            .then(function (response) {
-              vm.students = response.data;
+              $scope.scrapes = response.data;
            })
-        })
+           .catch(function(response) {
+                    $log.error('Error retrieving scrapes: ' + response.statusText);
+              $scope.errorText = $q.reject('Error retrieving scrapes.');
+           })
+     });
+
+   
  /*       
  <!--View HTML-->
- <button ng-click="midCtrl.reloadData()">Rescrape</button>
+ <button ng-click="$scope.reloadData()">Rescrape</button>
  */
+ 
